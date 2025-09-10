5973206444583839ef26ccf584d2774578d998ff
Pressense Fasani <camillefasani@gmail.com>
2025-09-10 14:36:33 +0200

    update: display feelings only on click for a less busy page

--- static/js/keep-toggle-open-and-display-selected-items.js
+++ static/js/keep-toggle-open-and-display-selected-items.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
     function isDesktop() {
         return window.innerWidth >= 768;
     }
-
+    
+    // Afficher les sentiments ou besoins sélectionnés
     function updateSelectedList() {
         listOfItems.innerHTML = "";
         
@@ -17,18 +18,42 @@ document.addEventListener("DOMContentLoaded", function() {
             const label = box.closest("label");
             li.textContent = label.textContent;
             listOfItems.appendChild(li);
-        });
-        
+            if (isDesktop()){
+                label.style.display = "block";
+            }
+        })
+
         // Masquer le message d'erreur s'il y a des cases cochées
         if (checkedBoxes.length > 0) {
             errorMessage.style.display = "none";
-        } 
-        
-        
+        }   
     }
 
+    //Laisser les familles ouvertes sur desktop et sur mobile si elles ont des items sélectionnés
     families.forEach((family) => {
         let boxes = family.querySelectorAll("input[type=checkbox]");
+        
+        //Faire apparaître/disparaître les sentiments ou besoin au clic sur la famille sur desktop
+        family.addEventListener("click", () => {
+            if (isDesktop()) {
+                const familyLabels = family.querySelectorAll("label");
+                familyLabels.forEach(label => {
+                    const checkbox = label.querySelector("input[type=checkbox]");
+                    if(checkbox.checked){
+                        label.style.display = "block";
+                    }
+                    
+                    if (label.style.display === "block") {
+                        // Ne cacher que si la checkbox n'est pas cochée
+                        if (!checkbox.checked) {
+                            label.style.display = "none";
+                        }
+                    } else {
+                        label.style.display = "block";
+                    }
+                });
+            }
+        });
 
         function hasCheckedBox() {
             return Array.from(boxes).some(b => b.checked);
@@ -71,14 +96,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 return Array.from(boxes).some(b => b.checked);
             }
             
+            // Réinitialiser les styles inline des labels
+            const familyLabels = family.querySelectorAll("label");
+            familyLabels.forEach(label => {
+                label.style.display = "";
+            });
+            
             if (isDesktop() || hasCheckedBox()) {
                 family.setAttribute("open", "");
+                label.style.display = "block";
             } else {
                 family.removeAttribute("open");
             }
         });
     });
 
-    // Initialiser la liste au chargement de la page
+    // Initialiser la liste et les labels cochés au chargement de la page
     updateSelectedList();
+
 });
